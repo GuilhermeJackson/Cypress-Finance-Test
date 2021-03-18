ff00dbed3a5ab9b6379a9e2b7511df2e61ef6ef3
GuilhermeLamim <guilherme.lamim@ambevtech.com.br>
2021-03-18 20:56:39 -0300

    inserindo produtos manualmente

--- cypress/integration/finances.spec.js
+++ cypress/integration/finances.spec.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress"/>
+import { format } from '../support/utils'
 
 context('Dev Dinances Agilizei', () => {
     beforeEach(() => {
@@ -55,5 +56,48 @@ context('Dev Dinances Agilizei', () => {
             .siblings() // busca no elemento irmãos
             .children('img[onclick*=remove]')
             .click();
+    });
+    it('Validar saldo com diversas transações', () => {
+        const entrada = 'Skol'
+        const saida = 'Patagonia'
+
+        cy.get('#transaction .button').click(); // id + classe
+        cy.get('#description').type(entrada); // id
+        cy.get('[name=amount').type(100); //atributos
+        cy.get('[type=date').type('2021-03-22'); //atributos
+        cy.get('button').contains('Salvar').click(); // tipo e valor
+        cy.get('#data-table tbody tr').should('have.length', 1);
+
+        cy.get('#transaction .button').click(); // id + classe
+        cy.get('#description').type(saida); // id
+        cy.get('[name=amount').type(-50); //atributos
+        cy.get('[type=date').type('2021-03-22'); //atributos
+        cy.get('button').contains('Salvar').click(); // tipo e valor
+
+        let incomes = 0
+        let expenses = 0
+        
+        cy.get('#data-table tbody tr')
+        .each(($el, index, $list) => {
+            cy.get($el).find('td.income, td.expense') //td.icome = colunas de entrada // td.expense = colunas de saida
+            .invoke('text') //capturando valores das colunas
+            .then(text => {
+                if(text.includes('-')) {
+                    expenses = expenses + format(text)
+                } else {
+                    incomes = incomes + format(text)
+                }
+                cy.log('Entradas', incomes)
+                cy.log('Saidas', expenses)
+            })
+        })
+        cy.get('#totalDisplay')
+        .invoke('text')
+        .then(text => {
+            cy.log('valor tatol', format(text))
+            let formattedTotalDisplay = format(text)
+            let expectedTotal = incomes + expenses
+            expect(formattedTotalDisplay).to.eq(expectedTotal);
+        });
     });
 });
